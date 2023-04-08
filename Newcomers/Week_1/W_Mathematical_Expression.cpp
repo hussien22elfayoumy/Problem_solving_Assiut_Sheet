0c8a088a74b1656af46de6dd6fad83779783f335
hussien22elfayoumy <hussienmedo36@gmail.com>
2023-04-09 01:00:27 +0200

    W

---
+++ Newcomers/Week_1/W_Mathematical_Expression.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a, b, c;
+  char s, q;
+
+  cin >> a >> s >> b >> q >> c; 
+
+  if (s == '+' && q == '=')
+  {
+    if (a + b == c)
+    cout << "Yes" << endl;
+    else 
+    cout << a + b << endl; 
+  }
+  else if (s == '-' && q == '=')
+  {
+    if (a - b == c)
+    cout << "Yes" << endl;
+    else 
+    cout << a - b << endl; 
+  }
+  else if (s == '*' && q == '=')
+  {
+    if (a * b == c)
+    cout << "Yes" << endl;
+    else 
+    cout << a * b << endl; 
+  }
+
+  return 0;
+}

884db201475ec6a9c032750b72a0d9a3215424d4
hussien22elfayoumy <hussienmedo36@gmail.com>
2023-04-08 21:01:43 +0200

    N

---
+++ Newcomers/Week_1/N_Char.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  char ch;
+  
+  cin >> ch;
+  if (ch >= 'a' && ch <= 'z')
+  cout << char(ch - 32) << endl;
+  else if (ch >= 'A' && ch <= 'Z')
+  cout << char(ch + 32) << endl;
+
+  return 0;
+}

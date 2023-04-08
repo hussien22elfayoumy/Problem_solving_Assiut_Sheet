2471c5255611fb4618bdb383280b948af2f7b896
hussien22elfayoumy <hussienmedo36@gmail.com>
2023-04-09 00:40:19 +0200

    V

---
+++ Newcomers/Week_1/V_Comparison.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a, b;
+  char s;
+  cin >> a >> s >> b;
+
+  switch (s)
+  {
+    case '=':
+    if (a == b)
+    cout << "Right" << endl;
+    else
+    cout << "Wrong" << endl;
+    break;
+    case '>':
+    if (a > b)
+    cout << "Right" << endl;
+    else
+    cout << "Wrong" << endl;
+    break;
+    case '<':
+    if (a < b)
+    cout << "Right" << endl;
+    else
+    cout << "Wrong" << endl;
+    break;
+    default:
+    return 0;
+  }
+  return 0;
+}

411e729b8ed6d0793b64a30e2271240522d91599
hussien22elfayoumy <hussienmedo36@gmail.com>
2023-04-08 20:20:17 +0200

    B

---
+++ Newcomers/Week_1/B_Basic_Data_Type.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x;
+  long long llx;
+  char ch;
+  float fx;
+  double dx;
+
+  cin >> x >> llx >> ch >> fx >> dx;
+  cout << x << "\n" << llx << "\n" << ch << "\n" << fx << "\n" << dx << "\n";
+  return 0;
+}

77ab735c2cf94696667a78837fb9560265197ccb
hussien22elfayoumy <hussienmedo36@gmail.com>
2023-04-20 17:36:40 +0200

    w

---
+++ Newcomers/Week_2/W_Shape3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+  int x = n;
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= n + i - 1; j++)
+    {
+      if (j + i >= n + 1)
+        cout << "*";
+      else
+        cout << " ";
+    }
+    cout << endl;
+  }
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= (2 * n) - i; j++)
+    {
+      if (j >= i)
+        cout << "*";
+      else
+        cout << " ";
+    }
+    cout << endl;
+  }
+  return 0;
+}

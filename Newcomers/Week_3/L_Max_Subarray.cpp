fcae21f9d56a5284780e0239e9d2a0d2d4db5978
hussien22elfayoumy <hussienmedo36@gmail.com>
2023-04-26 16:20:04 +0200

    L

---
+++ Newcomers/Week_3/L_Max_Subarray.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+      cin >> arr[i];
+    for (int i = 0; i < n; i++)
+      cout << arr[i] << " ";
+    for (int i = 0; i < n; i++)
+    {
+      int maxx = arr[i];
+      for (int j = i + 1; j < n; j++)
+      {
+        maxx = max(maxx, arr[j]);
+        cout << maxx << " ";
+      }
+    }
+    cout << endl;
+  }
+  return 0;
+}
